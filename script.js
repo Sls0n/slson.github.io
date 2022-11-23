77e27800b9f1d4b1fab17c6cd37deb834c4f85c9
Slson <silson0072@gmail.com>
2022-11-23 21:09:52 +0545

    updated

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-// Selectors
+'use strict';
 
+// Selectors
 const main = document.querySelector('.main');
 const header = document.querySelector('.header');
 const navHeight = header.getBoundingClientRect().height;
 
-// const starsBox = document.querySelector('.stars-box')
 const stars = document.querySelector('.stars-box');
 
 const skillsBtn = document.querySelector('.main__btn1');
 const skillsSection = document.querySelector('#skills');
 
-// console.log(Array.from(stars.children));
+const starsChildren = Array.from(stars.children);
 
 // Star Toggle
-
-// document.querySelector('.staricon').addEventListener('click', function () {
-//   let stars = document.querySelector('#stars');
-//   let stars3 = document.querySelector('#stars3');
-//   document.querySelector('#stars3');
-//   // if (
-//   //   stars.style.visibility === 'hidden' &&
-//   //   stars3.style.visibility === 'hidden'
-//   //   // For the toggle thing!
-//   // ) {
-//   //   stars.style.visibility = 'visible';
-//   //   stars3.style.visibility = 'visible';
-//   // } else {
-//   //   stars.style.visibility = 'hidden';
-//   //   stars3.style.visibility = 'hidden';
-//   // }
-
-// });
+document.querySelector('.header__icon').addEventListener('click', function (e) {
+  if (e.target.classList.contains('header--icon')) {
+    stars.classList.toggle('none');
+  }
+});
 
 // Scroll
-
 skillsBtn.addEventListener('click', function (e) {
   skillsSection.scrollIntoView({ behavior: 'smooth' });
 });
 
 // Sticky Navigation Implementation
-
 const stickyNav = function (entries) {
   const [entry] = entries;
   if (!entry.isIntersecting) {
@@ -51,7 +36,7 @@ const stickyNav = function (entries) {
 
 const mainObserver = new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: 0,
+  threshold: 0
   // rootMargin: '-20px'//`-${navHeight}px`
 });
 
